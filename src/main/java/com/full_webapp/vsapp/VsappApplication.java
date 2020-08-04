73a66d1718e162533aa992becab849ece3583081
Igor <lgorvysocky@gmail.com>
2020-08-04 20:29:38 +0200

    init

---
+++ src/main/java/com/full_webapp/vsapp/VsappApplication.java
@@ -0,0 +1,13 @@
+package com.full_webapp.vsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsappApplication {
+
+  public static void main(String[] args) {
+	SpringApplication.run(VsappApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/full_webapp/vsapp/VsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.full_webapp.vsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
